6369b90ddfbcc4fab73538ae7ffee4fd32567220
Marcus <Marcus@Lily.com>
2023-07-19 14:04:36 +0200

    Clean files

--- http/http.h
+++
@@ -1,9 +0,0 @@
-#include "esp_http_client.h"
-
-esp_http_client_handle_t http_init(void);
-void http_read(esp_http_client_handle_t client);
-void http_write(esp_http_client_handle_t client, char * buffer, int buffer_len);
-void http_cleanup(esp_http_client_handle_t client);
-void http_post(esp_http_client_handle_t client, char * buffer, int buffer_len);
-void http_fetch_headers(esp_http_client_handle_t client);
-void http_open(esp_http_client_handle_t client, int write_len);
--- temperature_sensor/temperature.h
+++
@@ -1,3 +0,0 @@
-void start_temp_sensor(void);
-void read_temp_sensor(float * result);
-void stop_temp_sensor(void);
